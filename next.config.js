d1d9c182497a8923ab8d646918ebfb54da9854c7
U Tikal <utikal@Us-MacBook-Pro.local>
2023-07-23 15:58:06 +0300

    initial files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+    // images: {
+    //     loader: 'custom',
+    //     loaderFile: './app/image.ts',
+    //   },
+}
 
 module.exports = nextConfig
